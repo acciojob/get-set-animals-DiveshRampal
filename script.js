c6085c4589a09a084caaef57bb591d9d8154f7d4
DiveshRampal <diveshrampal14@gmail.com>
2025-04-06 17:14:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
-//complete this code
-class Animal {}
+// Base Animal class
+class Animal {
+  // Constructor to store the species
+  constructor(species) {
+    this.species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter to retrieve the species
+  getSpecies() {
+    return this.species;
+  }
 
-class Cat extends Animal {}
+  // Method to make a sound
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
+
+// Dog class extending Animal
+class Dog extends Animal {
+  constructor(species) {
+    super(species); // Call the parent class constructor with species
+  }
+
+  // Method for barking
+  bark() {
+    console.log('woof');
+  }
+}
+
+// Cat class extending Animal
+class Cat extends Animal {
+  constructor(species) {
+    super(species); // Call the parent class constructor with species
+  }
+
+  // Method for purring
+  purr() {
+    console.log('purr');
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
